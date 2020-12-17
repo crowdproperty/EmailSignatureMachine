4beeaef3872ad95b48fcffad1944d629228828b9
Andrey Morozov <andrej.morozov@crowdproperty.com>
2020-12-17 13:32:35 +0000

    why not add some snow v12

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,45 @@ function generateSig() {
   var name = document.getElementById("full-name").value,
     title = document.getElementById("title").value,
     member = $('input[name="member"]:checked').val();
-  // phone = document.getElementById("phone").value;
+    phone = document.getElementById("phone").value;
 
   $searchBlock.find(".employee-name").text(name);
   $searchBlock.find(".employee-title").text(title);
 
-  if (member == "human") {
-    $searchBlock.find(".employee-number").text("020 3012 0161");
+//other no number
+if (phone == '' && member == "human"){
+  $searchBlock.find(".employee-number").text("020 3012 0161");
     $(".oi").attr(
       "src",
       "https://www.crowdproperty.com/assets/images/signature/bnr.png"
     );
-  } else {
+  }
+  // lender with nmbr
+  else if (phone != '' && member == "human"){ 
+    $searchBlock.find(".employee-number").text(phone);
+    $(".oi").attr(
+      "src",
+      "https://www.crowdproperty.com/assets/images/signature/bnr.png"
+    );
+  }
+  //brwr no number
+  else if (phone == '' && member != "human" ){
     $searchBlock.find(".employee-number").text("020 3012 0166");
     $(".oi").attr(
       "src",
       "https://www.crowdproperty.com/assets/images/signature/bnr_brwr.png"
     );
-  }
 
+  }
+  // brwr with  number
+  else if (phone != '' && member != "human"){ 
+    $searchBlock.find(".employee-number").text(phone);
+    $(".oi").attr(
+      "src",
+      "https://www.crowdproperty.com/assets/images/signature/bnr_brwr.png"
+    );
+  }
+  
   $("button").show();
 }
 clipboard.on("success", function (e) {
@@ -37,3 +57,5 @@ clipboard.on("success", function (e) {
 clipboard.on("error", function (e) {
   console.log(e);
 });
+
+ 
